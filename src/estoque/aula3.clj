8be99a1143a8725fdd6ddedc8f8c88ff42bbd50d
Erich Parmejano <erich.parmejano@nubank.com.br>
2022-07-22 16:18:08 -0300

    first classes

---
+++ src/estoque/aula3.clj
@@ -0,0 +1,77 @@
+(ns estoque.aula3)
+
+(defn valor-descontado
+  "Retorna o valor com desconto de 10% se o valor bruto for estritamente maior que 100."
+  [valor-bruto]
+  (if (> valor-bruto 100)
+    (let [taxa-de-desconto (/ 10 100)
+          desconto (* valor-bruto taxa-de-desconto)]
+      (- valor-bruto desconto))
+    valor-bruto))
+
+(println (valor-descontado 1000))
+
+(println (valor-descontado 100))
+
+; PREDICATE
+(defn aplica-desconto?
+  [valor-bruto]
+  (if (> valor-bruto 100)
+    true
+    false))
+
+(println (aplica-desconto? 1000))
+(println (aplica-desconto? 100))
+
+(defn valor-descontado
+  "Retorna o valor com desconto de 10% se o valor bruto for estritamente maior que 100."
+  [valor-bruto]
+  (if (aplica-desconto? valor-bruto)
+    (let [taxa-de-desconto (/ 10 100)
+          desconto (* valor-bruto taxa-de-desconto)]
+      (- valor-bruto desconto))
+    valor-bruto))
+
+(println (valor-descontado 1000))
+(println (valor-descontado 100))
+
+; PREDICATE
+(defn aplica-desconto?
+  [valor-bruto]
+  (println "chamando a versao redefinida")
+  (if (> valor-bruto 100)
+    true))
+
+(println (aplica-desconto? 1000))
+(println (aplica-desconto? 100))
+
+(defn valor-descontado
+  "Retorna o valor com desconto de 10% se o valor bruto for estritamente maior que 100."
+  [valor-bruto]
+  (if (aplica-desconto? valor-bruto)
+    (let [taxa-de-desconto (/ 10 100)
+          desconto (* valor-bruto taxa-de-desconto)]
+      (- valor-bruto desconto))
+    valor-bruto))
+
+(println (valor-descontado 1000))
+(println (valor-descontado 100))
+
+
+(defn aplica-desconto?
+  [valor-bruto]
+  (println "chamando a versao when")
+  (when (> valor-bruto 100)
+    true))
+
+(println (aplica-desconto? 1000))
+(println (aplica-desconto? 100))
+
+(defn aplica-desconto?
+  [valor-bruto]
+  (println "chamando a versao direta")
+  (> valor-bruto 100))
+
+
+(println (aplica-desconto? 1000))
+(println (aplica-desconto? 100))
--- src/estoque/core.clj
+++ src/estoque/core.clj
@@ -1,6 +1,4 @@
 (ns estoque.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(println "Bem vindo ao sistema de estoque")
+
